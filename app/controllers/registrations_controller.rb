ae922841065dae392dd7c9ea3f232100a61121b0
Yoshihiro Takahashi <tavehohdai@yahoo.co.jp>
2017-05-06 16:36:53 +0000

    20170507 Finish user edit action

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -11,4 +11,9 @@ private
         params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end    
 
+    def account_update_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+    end 
+
+
 end
